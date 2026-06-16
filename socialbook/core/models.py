6cf00ce954e0c29d7d8e3922726835349501e0d2
Rijo007 <rijovarghese225@gmail.com>
2026-06-16 23:43:30 +0530

    first commit

--- socialbook/core/models.py
+++ socialbook/core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Profile(models.Model):
+    
+    user=
+    id_user=
+    bio=
+    profileimg=models.ImageField(upload_to='media',default=)
--- socialbook/core/views.py
+++ socialbook/core/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+
+from django.contrib.auth.models import User,auth
+
+from django.contrib import messages
 
 # Create your views here.
 
@@ -6,3 +10,37 @@ from django.shortcuts import render
 def index(request):
     
     return render(request, 'index.html')
+
+
+
+def signup(request,id):
+    
+    if request.method=="POST":
+        
+        username=request.POST['firstname']
+        email=request.POST['email']
+        password1=request.POST['password1']    
+        password2=request.POST['password2']
+        
+        
+        if password1==password2:   
+            if User.objects.filter(email=email).exists():
+                messages.info(request,'email taken')
+                
+            elif  User.objects.filter(username=username).exists():
+                
+                messages.info(request,'username already taken')
+                
+            else:
+                
+                user=User.objects.create_user(username=username,password=password1)
+                user.save()    
+                
+        else:
+            
+            messages.info(request,"password not matching")  
+            return redirect('signup')  
+    else:        
+                 
+    
+        return render(request,'signup.html')
