9208da7fb0d14f6c640de6d5031d9d873d0a6e0a
Rijo007 <rijovarghese225@gmail.com>
2026-06-16 23:57:24 +0530

    first commit

--- socialbook/core/models.py
+++ socialbook/core/models.py
@@ -1,10 +1,16 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
 
 # Create your models here.
 
+
+User=get_user_model()
+
+
 class Profile(models.Model):
     
-    user=
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
     id_user=
     bio=
-    profileimg=models.ImageField(upload_to='media',default=)
+    profileimg=models.ImageField(upload_to='profile_images')
