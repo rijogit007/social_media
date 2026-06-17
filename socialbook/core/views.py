a17ef570455f8fe8d99047a3e7c2be7bb2a4d7f3
Rijo007 <rijovarghese225@gmail.com>
2026-06-18 00:11:17 +0530

    first commit

--- socialbook/core/views.py
+++ socialbook/core/views.py
@@ -16,35 +16,39 @@ def home(request):
     return render (request,'home.html')
 
 
+def signup(request):
 
-def signup(request,id):
-    
-    if request.method=="POST":
-        
-        username=request.POST['firstname']
-        email=request.POST['email']
-        password1=request.POST['password1']    
-        password2=request.POST['password2']
-        
-        
-        if password1==password2:   
+    if request.method == "POST":
+
+        user_name = request.POST.get('user_name')
+        first_name = request.POST.get('first_name')
+        email = request.POST.get('email')
+        password1 = request.POST.get('password1')
+        password2 = request.POST.get('password2')
+
+        if password1 == password2:
+
             if User.objects.filter(email=email).exists():
-                messages.info(request,'email taken')
-                
-            elif  User.objects.filter(username=username).exists():
-                
-                messages.info(request,'username already taken')
-                
+                messages.info(request, 'Email already taken')
+                return redirect('signup')
+
+            elif User.objects.filter(username=user_name).exists():
+                messages.info(request, 'Username already taken')
+                return redirect('signup')
+
             else:
-                
-                user=User.objects.create_user(username=username,password=password1)
-                user.save()    
-                
+                user = User.objects.create_user(
+                    username=user_name,
+                    first_name=first_name,
+                    email=email,
+                    password=password1
+                )
+
+                user.save()
+                return redirect('login')
+
         else:
-            
-            messages.info(request,"password not matching")  
-            return redirect('signup')  
-    else:        
-                 
-    
-        return render(request,'signup.html')
+            messages.info(request, 'Passwords do not match')
+            return redirect('signup')
+
+    return render(request, 'signup.html')
